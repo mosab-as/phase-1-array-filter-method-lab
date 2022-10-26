a7aaa930976e989fe1ca531ac1342a6282c4443c
Golden-Man <Golden-Man@users.noreply.github.com>
2022-10-26 04:34:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(array,name)
+{
+  return array.filter((value) => value === name || value === name.toLowerCase() )
+}
+
+function fuzzyMatch(array,twoChar)
+{
+  return array.filter((value) => value[0] === twoChar[0] && value[1] === twoChar[1])
+}
+function matchName(array,name)
+{
+  return array.filter((value) => value.name === name )
+}
